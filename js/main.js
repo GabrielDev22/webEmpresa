0c5b6016734bebc36f12860a920afc0f30bc2cf5
unknown <gabrieltorresmora2@gmail.com>
2022-07-24 18:55:57 -0300

    haciendo el slider y parte de la seccion de servicio

--- js/main.js
+++ js/main.js
@@ -47,6 +47,36 @@ const deleteStyleHeight = () =>{
 
 })();
 
+/*Slider Nuestros Clientes */
+
+let slider = document.querySelector(".clientes-contenedor")
+let sliderIndividual = document.querySelectorAll(".clientes__slider")
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 5000;
+
+window.addEventListener('resize', function(){
+    width = sliderIndividual[0].clientWidth;
+})
+
+setInterval(function(){
+    slides();
+},intervalo);
+
+function slides(){
+    slider.style.transform = "translate("+(-width*contador)+"px)";
+    slider.style.transition = "transform .8s";
+    contador++;
+
+    if(contador == sliderIndividual.length){
+        setTimeout(function(){
+            slider.style.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+            contador=1;
+        },8000)
+    }
+}
+
 
 
 
